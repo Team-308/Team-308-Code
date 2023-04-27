ef32c908ec286b677f4fb5a0651c3725be783a87
asarras <122954007+asarras@users.noreply.github.com>
2023-04-26 18:36:01 -0700

    Update main.c

--- pic/main.c
+++ pic/main.c
@@ -18,17 +18,82 @@
         Driver Version    :  2.00
 */
 
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
 
 #include "mcc_generated_files/mcc.h"
-
+#pragma LVP = ON
 /*
                          Main application
  */
+
+uint8_t data1;
+uint8_t data12;
+int ii;
+float value;
+
+bool loop = true;
+
+void EUSART1intrrupt(void){
+    EUSART1_Receive_ISR();
+
+    if(EUSART1_is_rx_ready()){
+        data1 = EUSART1_Read();
+        data12 = EUSART1_Read();
+        while(loop){
+            if(EUSART1_is_tx_ready()){
+                loop = false;
+            }
+        }
+        loop = true;
+        EUSART1_Write(data1);
+        EUSART1_Write(data12);
+        while(loop){
+            if(EUSART1_is_tx_done()){
+                loop = false;
+            }
+        }
+    }
+    
+    LED_0_Toggle();
+}
+
 void main(void)
 {
+    volatile uint8_t rxData;
+    uint8_t humid;
+    uint8_t wind;
+    int wind_V;
+    int humid_V;
     // Initialize the device
     SYSTEM_Initialize();
-
+    TMR2_Initialize();
+    TMR2_Start();
+    SPI1_Initialize();
+    SPI2_Initialize();
+    EUSART1_Initialize();
+    PWM4_Initialize();
+    
     // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
     // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
     // Use the following macros to:
@@ -44,47 +109,71 @@ void main(void)
 
     // Disable the Peripheral Interrupts
     //INTERRUPT_PeripheralInterruptDisable();
-    /*
-     * uint_8 tempHigh = ...; //relatively high temperature
-     * uint_8 tempLow = ...; //relatively low temperature
-     * uint_8 humHigh = ...; //relatively high humidity
-     * uint_8 humLow = ...; //relatively low humidity
-     * uint_8 windHigh
-     * 
-     * danger(int level, boolean reset){
-     *      //add danger level;
-     *      //move stepper according to danger
-     * }
-     */
+    
+    EUSART1_SetRxInterruptHandler(EUSART1intrrupt);
 
     while (1)
     {
+        wind_V = Wind_Pin_GetValue();
+        humid_V = Humid_Pin_GetValue();
+        
+        if(wind_V && humid_V>0){
+            if(wind_V>3.17){
+                SS_pin_SetHigh();
+                DIR_Pin_SetHigh();
+            }else{
+                SS_pin_SetHigh();
+                DIR_Pin_SetLow();
+            }
+        }else{
+                SS_pin_SetHigh();
+        }
+        
         // Add your application code
-        /*
-         * if (reset button press){
-         *      danger(0,true);
-         * }
-         * 
-         * windSpeed = Read ANA0 // The wind speed
-         * humidity = Read ANA1 //The humidity
-         * temperature = Read ANA2 //The temperature
-         * 
-         * windSpeedCalc = ... //From input value to calculated wind speed
-         * humidityCalc = ... //From input value to calculated humidity
-         * temperature = ... //From input value to calculated temperature
-         * 
-         * if (temperature > tempHigh && humidityCalc < humLow){
-         *      danger(3, false);
-         * }
-         * if (temperature < tempLow && humidityCalc > humHigh){
-         *      danger(2, false);
-         * }
-         * if (windSpeedCalc > windHigh){
-         *      danger(1, false);
-         * }
-         * 
-         * dangerLevelCalc=...;
-         */
+        wind = SPI1_ReadByte();
+        
+        humid = SPI2_ReadByte();
+        // Logic to echo received data
+            if(EUSART1_is_rx_ready())
+            {
+                rxData = EUSART1_Read();
+                if(EUSART1_is_tx_ready())
+                {
+                    EUSART1_Write(rxData);
+                }
+            }
+        
+            uint8_t dir1 = 0b11001111;
+            uint8_t dir2 = 0b11001101;
+            uint8_t receive;
+
+        SPI1_Open(SPI1_DEFAULT);
+        SS_pin_SetLow();
+        printf("Send: %u\r\n",dir1);
+
+        receive = SPI1_ExchangeByte(dir1);
+        printf("Receive: %u\r\n",receive);
+        SS_pin_SetHigh();
+        SPI1_Close();
+
+        
+
+        SPI1_Open(SPI1_DEFAULT);
+        SS_pin_SetLow();
+        printf("Send: %u\r\n",dir2);
+
+        receive = SPI1_ExchangeByte(dir2);
+        printf("Receive: %u\r\n",receive);
+        SS_pin_SetHigh();
+        SPI1_Close();
+        
+        if(ii>=100){
+            ii=0;
+        }
+        value= (float) ii/10;
+        printf("W: %u + H: %u  \r\n", wind, humid);
+        
+        
     }
 }
 /**
